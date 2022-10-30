292a1e0a9d94ddbfb828fc85d9d19d325d016970
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2022-10-30 22:34:49 +0800

    Update bitping.sh

--- bitping.sh
+++ bitping.sh
@@ -98,14 +98,14 @@ if [ $SYSTEM = "CentOS" ]; then
     if [ $ARCHITECTUREH = "amd64" ]; then
         rm -rf *bitping*
         yellow "Building"
-        rm -rf bitping-node-amd64-linux
+        rm -rf bitping-node-amd64-linux*
         wget https://github.com/spiritLHLS/bitping-one-click-command-installation/raw/main/bitping-node-amd64-linux
         chmod 777 bitping-node-amd64-linux
         ./bitping-node-amd64-linux --server --email "$EMAIL" --password "$PASSWORD"
     else
         rm -rf *bitping*
         yellow "Building"
-        rm -rf bitping-node-armv7-linux
+        rm -rf bitping-node-armv7-linux*
         wget https://github.com/spiritLHLS/bitping-one-click-command-installation/raw/main/bitping-node-armv7-linux
         chmod 777 bitping-node-armv7-linux
         ./bitping-node-armv7-linux --server --email "$EMAIL" --password "$PASSWORD"
@@ -118,14 +118,14 @@ else
     if [ $ARCHITECTUREH = "amd64" ]; then
         rm -rf *bitping*
         yellow "Building"
-        rm -rf bitping-node-amd64-linux
+        rm -rf bitping-node-amd64-linux*
         wget https://github.com/spiritLHLS/bitping-one-click-command-installation/raw/main/bitping-node-amd64-linux
         chmod 777 bitping-node-amd64-linux
         ./bitping-node-amd64-linux --server --email "$EMAIL" --password "$PASSWORD"
     else
         rm -rf *bitping*
         yellow "Building"
-        rm -rf bitping-node-armv7-linux
+        rm -rf bitping-node-armv7-linux*
         wget https://github.com/spiritLHLS/bitping-one-click-command-installation/raw/main/bitping-node-armv7-linux
         chmod 777 bitping-node-armv7-linux
         ./bitping-node-armv7-linux --server --email "$EMAIL" --password "$PASSWORD"
